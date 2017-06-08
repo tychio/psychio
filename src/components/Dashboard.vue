989d94e6095b577a743a30c8cd306bd5aa3d5609
Tychio <code@tychio.net>
2017-06-08 21:36:46 +0800

    move to Timeline as a single component

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,37 +4,21 @@
     <h1>PsychIO</h1>
     <button class="btn" @click="play()">Play</button>
   </section>
-  <section class="container" ref="container">
-    <div class="timeline">
-      <div class="timeline-block" v-for="(item,index) in outputs">
-        <div v-if="item.type === 'img-audio'">
-          <img :src="item.url" height="100">
-          <div class="showcase" v-if="index === currentIndex">
-            <img :src="item.url">
-          </div>
-          <Recorder 
-            :active="index === currentIndex" 
-            :item="item"
-          ></Recorder>
-        </div>
-        <div>
-          <input type="number" v-model="item.period">
-        </div>
-      </div>
-    </div>
+  <section class="container">
+    <Timeline :list="list" :currentIndex="currentIndex"></Timeline>
   </section>
 </main>
 </template>
 
 <script>
-import Recorder from './recorder'
+import Timeline from './timeline'
 
 export default {
   name: 'Dashboard',
   data () {
     let data = {
       now: 0,
-      outputs: [
+      list: [
         {
           type: 'img-audio',
           url: 'http://pic.baike.soso.com/ugc/baikepic2/4691/20150114182042-1266615931.jpg/300',
@@ -54,7 +38,7 @@ export default {
   computed: {
     duration: function () {
       let duration = 0
-      this.outputs.forEach(function (item) {
+      this.list.forEach(function (item) {
         duration += item.period
       })
       return duration
@@ -62,8 +46,8 @@ export default {
     currentIndex: function () {
       let current
       let passTime = this.now
-      for (let index = -1; index < this.outputs.length; index++) {
-        let item = this.outputs[index + 1]
+      for (let index = -1; index < this.list.length; index++) {
+        let item = this.list[index + 1]
         if (passTime <= 0) {
           current = index
           break
@@ -87,7 +71,7 @@ export default {
     }
   },
   components: {
-    Recorder
+    Timeline
   }
 }
 </script>
@@ -97,33 +81,6 @@ export default {
 .container {
   overflow: hidden;
   margin: 10px;
-}
-
-.showcase {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-}
-
-.showcase img {
-  width: 100%;
-  height: 100%;
-}
-
-.timeline {
-}
-
-.timeline-block {
-  display: block;
-  box-sizing: border-box;
-  border: 1px solid #333;
-}
-
-.timeline-block img {
-  display: block;
-  margin: 0 auto;
 }
 
 .btn {
