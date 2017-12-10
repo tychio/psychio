ba700836df4449ad64bb758bf56b434b6d594b23
Tychio <code@tychio.net>
2017-12-11 00:58:13 +0800

    push page

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../release/index.html'),
     assetsRoot: path.resolve(__dirname, '../release'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/PsychIO/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
